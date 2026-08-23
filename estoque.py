143bdff662a1cad4a83d37f1440fe6242278cf84
Ana Dev <ana@exemplo.com>
2026-08-23 21:54:49 +0000

    feat: baixa de estoque

--- estoque.py
+++ estoque.py
@@ -4,3 +4,10 @@ PRODUTOS = []
 def adicionar(nome, qtd):
     PRODUTOS.append({"nome": nome, "qtd": qtd})
     return PRODUTOS
+
+
+def baixa(nome, quantidade):
+    for p in PRODUTOS:
+        if p["nome"] == nome:
+            p["qtd"] -= quantidade
+    return PRODUTOS
